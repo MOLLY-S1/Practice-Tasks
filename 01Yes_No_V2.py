540229a4386838fe8052c52a46df65a306e6b11e
MOLLY-S1 <sankeym@middleton.school.nz>
2022-03-29 14:57:20 +1300

    first versions of Yes/No checker

---
+++ 01Yes_No_V2.py
@@ -0,0 +1,17 @@
+# Ask user if they have played before
+show_intructions = input("have you played before? :").lower()
+
+
+# If they say yes, output 'Program Continues'
+if show_intructions == 'yes' or show_intructions == 'y':
+    print("program continues")
+
+
+# If they say no, output 'Display instructions'
+elif show_intructions == 'no' or show_intructions == 'n':
+    print('show instructions')
+
+
+# Otherwise - show error
+else:
+    print("please enter 'yes' or 'no'")
