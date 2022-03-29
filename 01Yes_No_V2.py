2cd9701f7ca83b1da9e1284e78a12065d4a5c7e8
MOLLY-S1 <sankeym@middleton.school.nz>
2022-03-29 15:30:57 +1300

    update of Yes/No V2

--- 01Yes_No_V2.py
+++ 01Yes_No_V2.py
@@ -1,17 +1,24 @@
-# Ask user if they have played before
-show_intructions = input("have you played before? :").lower()
+# More Efficiant testing with loops
+
+show_instructions = ""
+while show_instructions != 'x':
+
+    # Ask user if they have played before
+    show_intructions = input("have you played before? :").lower()
 
 
-# If they say yes, output 'Program Continues'
-if show_intructions == 'yes' or show_intructions == 'y':
-    print("program continues")
+    # If they say yes, output 'Program Continues'
+    if show_intructions == 'yes' or show_intructions == 'y':
+        print("program continues")
 
 
-# If they say no, output 'Display instructions'
-elif show_intructions == 'no' or show_intructions == 'n':
-    print('show instructions')
+    # If they say no, output 'Display instructions'
+    elif show_intructions == 'no' or show_intructions == 'n':
+        print('show instructions')
 
 
-# Otherwise - show error
-else:
-    print("please enter 'yes' or 'no'")
+    # Otherwise - show error
+    else:
+        print("please enter 'yes' or 'no'")
+
+    print(f"you entered '{show_intructions}'")
